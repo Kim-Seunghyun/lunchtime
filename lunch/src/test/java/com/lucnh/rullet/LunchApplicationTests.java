2caa955362864f8a1e779865ffa22564cb42ebe4
Kim <sialskdn@gmail.com>
2022-09-28 23:34:51 +0900

    🎨 최초 프로젝트 생성
    Vue2+Typescript, SpringBoot

---
+++ lunch/src/test/java/com/lucnh/rullet/LunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucnh.rullet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
